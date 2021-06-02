9ffd27893358d134d5ee401f07b2e8b7fb430b96
Handy <handy.kwok@gmail.com>
2021-06-02 14:50:12 +0700

    Initial Commit

---
+++ MonsterList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MonsterList
+//
+//  Created by Handy Handy on 02/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
